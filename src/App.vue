90faaffe256ef2c49ebcd85fce39418f8f8d78a0
Geoff Sargison <gsarge.inzid@gmail.com>
2019-03-18 12:21:08 +1300

    added base app framework to build from, as well as some assets from home page design work

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home";
+
+export default {
+  name: "App",
+  components: {
+  }
+};
+</script>
+
+<style>
+</style>
