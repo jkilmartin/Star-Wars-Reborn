fb4204f7c6cc3ba60191d4429d17f5f9e43d3e61
Jeremy kwai leong Sim <845872974@dov2-sc-21.local>
2019-03-20 16:58:04 +1300

    Adding nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <MyNav />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MyNav from "./components/MyNav";
 
 export default {
   name: "App",
   components: {
+    MyNav
   }
 };
 </script>
